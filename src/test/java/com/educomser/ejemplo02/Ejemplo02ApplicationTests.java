769c9a274a5b51cb5106e97d827be94445d9732f
rusok <rusok.software@gmail.com>
2026-09-25 20:15:39 -0400

    Configurando SonarQube

--- src/test/java/com/educomser/ejemplo02/Ejemplo02ApplicationTests.java
+++ src/test/java/com/educomser/ejemplo02/Ejemplo02ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.educomser.ejemplo02;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.educomser.ejemplo02.controladores.PruebaController;
 
 @SpringBootTest
 class Ejemplo02ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		PruebaController pruebaController = new PruebaController();
+		String despedida = pruebaController.otroEndpoint();
+		assertEquals("Hasta la siguiente semana!!", despedida);
 	}
 
 }
